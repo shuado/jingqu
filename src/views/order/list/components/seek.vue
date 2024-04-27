33e71948182eb0efe392ad4674ad779e3ba490d3
shwado <shd_cn@163.com>
2024-04-27 20:44:45 +0800

    搜索

--- src/views/order/list/components/seek.vue
+++ src/views/order/list/components/seek.vue
@@ -5,7 +5,7 @@
 -->
 <template>
     <el-row :gutter="20">
-        <el-col :span="10">
+        <el-col :span="8">
             <el-radio-group v-model="seekData.value1">
                 <el-radio-button label="今天" value="1" />
                 <el-radio-button label="昨天" value="2" />
@@ -15,13 +15,13 @@
                 <el-radio-button label="本月" value="6" />
             </el-radio-group>
         </el-col>
-        <el-col :span="6">
-            <el-date-picker v-model="seekData.value2" type="daterange" range-separator="To" start-placeholder="开始时间" end-placeholder="结束时间" />
+        <el-col :span="8">
+            <el-date-picker v-model="seekData.value2" type="daterange" range-separator="-" start-placeholder="开始时间" format="YYYY-MM-DD" value-format="YYYY-MM-DD" end-placeholder="结束时间" @change="handleCheckChange" />
         </el-col>
         <el-col :span="6">
             <el-input v-model="seekData.value3" style="margin-left: 24px" placeholder="订单号">
                 <template #append>
-                    <el-button :icon="Search" />
+                    <el-button :icon="Search" @click="seekClick" />
                 </template>
             </el-input>
         </el-col>
@@ -29,10 +29,60 @@
 </template>
 <script setup>
 import { Search } from '@element-plus/icons-vue';
+import { dayjs } from 'element-plus';
 
 const seekData = ref({
     value1: '',
     value2: '',
     value3: '',
 });
+
+watch(
+    () => seekData.value.value1,
+    (val) => {
+        if (val) {
+            switch (val) {
+                case '1':
+                    // 获取当前时间
+                    seekData.value.value2 = [dayjs().format('YYYY-MM-DD 00:00:00'), dayjs().format('YYYY-MM-DD 23:59:59')];
+                    break;
+
+                case '2':
+                    // 获取昨天时间
+                    seekData.value.value2 = [dayjs().subtract(1, 'day').format('YYYY-MM-DD 00:00:00'), dayjs().subtract(1, 'day').format('YYYY-MM-DD  23:59:59')];
+                    break;
+
+                case '3':
+                    // 获取最近一周时间
+                    seekData.value.value2 = [dayjs().subtract(7, 'day').format('YYYY-MM-DD 00:00:00'), dayjs().subtract(1, 'day').format('YYYY-MM-DD   23:59:59')];
+                    break;
+
+                case '4':
+                    // 获取最近一个月时间
+                    seekData.value.value2 = [dayjs().subtract(30, 'day').format('YYYY-MM-DD 00:00:00'), dayjs().subtract(1, 'day').format('YYYY-MM-DD  23:59:59')];
+                    break;
+                case '5':
+                    // 获取本周时间
+                    seekData.value.value2 = [dayjs().day(1).format('YYYY-MM-DD 00:00:00'), dayjs().day(7).format('YYYY-MM-DD  23:59:59')];
+                    break;
+                case '6':
+                    // 获取本月时间
+                    seekData.value.value2 = [dayjs().startOf('month').format('YYYY-MM-DD 00:00:00'), dayjs().endOf('month').format('YYYY-MM-DD  23:59:59')];
+                    break;
+                default:
+                    break;
+            }
+            console.log(seekData.value.value2);
+        }
+    },
+);
+
+const emit = defineEmits(['seekClick']);
+const seekClick = () => {
+    emit('seekClick', seekData.value);
+};
+
+const handleCheckChange = () => {
+    seekData.value.value1 = '';
+};
 </script>
